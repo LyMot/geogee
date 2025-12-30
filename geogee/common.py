7e5083c287dbbe710e385b8e8dbf645ca3b33220
LyMot <lymot101@gmail.com>
2025-12-30 20:43:08 +0530

    commit common

--- geogee/common.py
+++ geogee/common.py
@@ -1,7 +1,4 @@
-"""The common module contains common functions and classes used by the other modules.
+"""
+The common module with some common functions of ipyleaflet and folium.
 """
 
-def hello_world():
-    """Prints "Hello World!" to the console.
-    """
-    print("Hello World!")
